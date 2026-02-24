3d01fc9df73494c8f942c351ba267d70177098ce
Tristan Lacombe <tlacombevulca@outlook.fr>
2026-02-24 10:39:36 +0100

    fix(app): Fixed Create input

--- src/components/CreateZone.jsx
+++ src/components/CreateZone.jsx
@@ -1,19 +1,24 @@
-import './CreateZone.css';
+import { useState } from "react";
+import "./CreateZone.css";
 
-export const CreateZone = ({ newObjective, handleChange, handleAddGoal }) => {
+export const CreateZone = ({ handleChange, handleAddGoal }) => {
+    const [value, setValue] = useState("");
+
+    const handleInputChange = (e) => {
+        setValue(e.target.value);
+        handleChange(e.target.value);
+    };
+
     return (
-        <div className="create-zone">
+        <div className='create-zone'>
             <input
-                className="create-zone__input"
-                type="text"
-                value={newObjective}
-                onChange={(e) => handleChange(e.target.value)}
-                placeholder="Entrez un nouvel objectif"
+                className='create-zone__input'
+                type='text'
+                value={value}
+                onChange={handleInputChange}
+                placeholder='Entrez un nouvel objectif'
             />
-            <button
-                className="create-zone__button"
-                onClick={handleAddGoal}
-            >
+            <button className='create-zone__button' onClick={handleAddGoal}>
                 Ajouter
             </button>
         </div>
